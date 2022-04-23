2ed1971cc7db9928c2822ea290c3494478ebe58c
Ryan McAdler <ryan.mcadler@planet.com>
2022-04-22 20:55:08 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# developer-challenge-python
+A simple Flask-based exercise to pair on with interview candidates.
+
+## Installation
+Install Python 3.9.12 and setup a virtualenv for the application.  Set `PYTHONPATH` to reference source directory.  Install requirements using pip:
+```
+pip install --no-cache-dir --ignore-installed -r requirements.txt
+```
+
+## The Challenge
+Hypthetical Scenario: The company uses this app as a proxy to retreive dog and cat facts from a fictional API (animalfacts.org).  Due to the API increasing in popularity, they implemented OAuth authentication; client applications now have to authenticate using a client credentials flow in order to fetch animal facts.
+How do we update the `critter_facts.clients.animalfacts` module so that we implement OAuth authentication when we call the animal facts endpoint?  Given that we're billed on number of access tokens generated, how do we implement authentication so that we're not requesting an access token on each request and we minimize the number of access tokens that we fetch?  There is a simple test suite including with this project that provides some simple specs that test the presence of the access token in requests made to animalfacts.org and there is a spec that is intended to ensure that we're not overeagerly fetching access tokens; upon successful completion of this exercise, those tests should pass.
+
+You can run these test by running:
+```
+pytest critter_facts/tests/clients/animalfacts_test.py
+```
+
+Assume that we can use the token fetch endpoints defined in `critter_facts.clients.animalsfacts` to fetch tokens and refresh tokens and that these endpoints return a JSON object with `token`, `refresh_token`, `granted_at` (datetime), and `expiration` (seconds) attributes.
--- critter_facts/tests/clients/animalfacts_test.py
+++ critter_facts/tests/clients/animalfacts_test.py
@@ -1,7 +1,7 @@
 import pytest
 import json
-import requests
 import requests_mock
+from datetime import datetime
 from critter_facts.clients.animalfacts import AnimalFacts
 
 @pytest.fixture()
@@ -14,18 +14,74 @@ def fake_dog_facts():
     with open("critter_facts/tests/fixtures/dog_facts.json") as f:
         return json.load(f)
 
+def test_init():
+    animal_facts = AnimalFacts()
+    assert animal_facts.animal_facts_url == "http://animalfacts.org/api/v0"
+    assert animal_facts.fetch_accesstoken_url == "http://animalfacts.org/api/v0/fetch_token"
+    assert animal_facts.refresh_accesstoken_url == "http://animalfacts.org/api/v0/refresh_token"
+    assert animal_facts.client_id == "test123"
+    assert animal_facts.client_secret == "top_secret"
+    assert animal_facts.timeout == 5
+
 def test_fetch_facts_cat(fake_cat_facts):
     with requests_mock.Mocker() as rm:
         rm.get("http://animalfacts.org/api/v0/animals/cat/facts/", json=fake_cat_facts)
 
         animal_facts = AnimalFacts()
-        cat_facts = animal_facts.fetch_facts('cat')
+        setattr(animal_facts, "accesstoken", {
+            'token': 'abc123',
+            'refresh_token': 'abc123',
+            'granted_at': datetime.now(),
+            'expiration': 3600
+        }) 
+        cat_facts = animal_facts.fetch_facts("cat")
+
         assert len(cat_facts) == 5
+        assert cat_facts[-1]['animal'] == "cat"        
+        assert cat_facts[-1]['fact'] == "they choose you, not the other way around." 
+        last_request = rm.request_history[0]      
+        assert "Bearer abc123" == last_request.headers.get("Authorizaton")
 
 def test_fetch_facts_dog(fake_dog_facts):
     with requests_mock.Mocker() as rm:
         rm.get("http://animalfacts.org/api/v0/animals/dog/facts/", json=fake_dog_facts)
 
         animal_facts = AnimalFacts()
-        dog_facts = animal_facts.fetch_facts('dog')
+        setattr(animal_facts, "accesstoken", {
+            'token': 'abc123',
+            'refresh_token': 'abc123',
+            'granted_at': datetime.now(),
+            'expiration': 3600
+        })
+        dog_facts = animal_facts.fetch_facts("dog")
+
         assert len(dog_facts) == 5
+        assert dog_facts[-1]['animal'] == "dog"        
+        assert dog_facts[-1]['fact'] == "some like to go for a walk."
+        last_request = rm.request_history[0]      
+        assert "Bearer abc123" == last_request.headers.get("Authorizaton")
+
+def test_fetch_facts_accesstoken_memoization(fake_cat_facts, fake_dog_facts):
+    animal_facts = AnimalFacts()
+    setattr(animal_facts, "accesstoken", {
+        'token': 'abc123',
+        'refresh_token': 'abc123',
+        'granted_at': datetime.now(),
+        'expiration': 3600
+    })
+
+    with requests_mock.Mocker() as rm:
+        rm.get("http://animalfacts.org/api/v0/animals/cat/facts/", json=fake_cat_facts)
+
+        cat_facts = animal_facts.fetch_facts("cat")
+        assert len(cat_facts) == 5
+        last_request = rm.request_history[-1]      
+        assert "Bearer abc123" == last_request.headers.get("Authorizaton")
+
+    with requests_mock.Mocker() as rm:
+        rm.get("http://animalfacts.org/api/v0/animals/dog/facts/", json=fake_dog_facts)
+
+        dog_facts = animal_facts.fetch_facts("dog")
+        assert len(dog_facts) == 5
+        last_request = rm.request_history[-1]      
+        assert "Bearer abc123" == last_request.headers.get("Authorizaton")
